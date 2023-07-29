6cabe35c4f7fb26b4cc9f4fc6dc0826c9ce0af98
Yasir558 <110908280+Yasir558@users.noreply.github.com>
2023-07-29 15:02:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function onBulb(){
+var img = document.getElementById("off")
+img.src = "../800px_COLOURBOX15768579.jpg"
+}
+
+function offBulb(){
+  var img = document.getElementById("off")
+  img.src ="../photo-1529310399831-ed472b81d589.jpg"
+  }
